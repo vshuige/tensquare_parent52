4287c26f4504af385b470ed0d4737c4de1b5ac66
liangzhihui <1498730879@qq.com>
2018-12-28 20:12:12 +0800

    配置文件git托管，即将要删配置文件

--- tensquare_manager/src/main/java/com/tensquare/manager/filter/ManagerFilter.java
+++ tensquare_manager/src/main/java/com/tensquare/manager/filter/ManagerFilter.java
@@ -1,8 +1,14 @@
 package com.tensquare.manager.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import util.JwtUtil;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Author: liangzhihui
@@ -10,6 +16,9 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class ManagerFilter extends ZuulFilter{
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     /**
      * 请求前执行pre请求后执行post
@@ -47,6 +56,46 @@ public class ManagerFilter extends ZuulFilter{
     @Override
     public Object run() throws ZuulException {
         System.out.println("经过后台网关过滤器了");
+        // 得到上下文
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        // 得到域
+        HttpServletRequest request = currentContext.getRequest();
+
+        if(request.getMethod().equals("OPTIONS")){
+            return null;
+        }
+        if(request.getRequestURI().indexOf("login")>0){
+            return null;
+        }
+
+
+        // 得到请求头
+        String header = request.getHeader("Authorization");
+        if (header!=null && !"".equals(header)){
+            if(header.startsWith("Bearer")){
+                String token = header.substring(7);
+                try {
+                    Claims claims = jwtUtil.parseJWT(token);
+                    String roles = (String) claims.get("roles");
+                    if("admin".equals(roles)){
+                        //把信息头传下去，并且放行
+                        currentContext.addZuulRequestHeader("Authorization",header);
+                        return null;
+
+                    }
+
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                    currentContext.setSendZuulResponse(false); //终止运行
+                }
+            }
+        }
+
+        currentContext.setSendZuulResponse(false); //终止运行
+        currentContext.setResponseStatusCode(403);
+        currentContext.setResponseBody("权限不足");
+        currentContext.getResponse().setContentType("text/html;charset=utf-8");
         return null;
     }
 }
